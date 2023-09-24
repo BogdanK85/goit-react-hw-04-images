2067905ba662ad845b3eb6a5eb879061f1cdc72e
BogdanK85 <kobakbogdan@gmail.com>
2023-09-24 17:27:16 +0300

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,36 @@
-// import { useEffect } from 'react';
-// import { createPortal } from 'react-dom';
-// import { ModalStyle, OverlayStyle } from './Modal.styled';
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { ModalStyle, OverlayStyle } from './Modal.styled';
 
-// const modalRoot = document.querySelector('#modal-root');
+const modalRoot = document.querySelector('#modal-root');
 
-// const Modal = ({ largeImageURL, onCloseModal }) => {
-//   const onBackdropClick = event => {
-//     if (event.target === event.currentTarget) {
-//       onCloseModal();
-//     }
-//   };
+const Modal = ({ largeImageURL, onCloseModal }) => {
+  const onBackdropClick = event => {
+    if (event.target === event.currentTarget) {
+      onCloseModal();
+    }
+  };
 
-//   useEffect(() => {
-//     const handlePressEscape = event => {
-//       if (event.code === 'Escape') {
-//         onCloseModal();
-//       }
-//     };
+  useEffect(() => {
+    const handlePressEscape = event => {
+      if (event.code === 'Escape') {
+        onCloseModal();
+      }
+    };
 
-//     document.addEventListener('keydown', handlePressEscape);
-//     return () => {
-//       document.removeEventListener('keydown', handlePressEscape);
-//     };
-//   }, [onCloseModal]);
+    document.addEventListener('keydown', handlePressEscape);
+    return () => {
+      document.removeEventListener('keydown', handlePressEscape);
+    };
+  }, [onCloseModal]);
 
-//   return createPortal(
-//     <OverlayStyle onClick={onBackdropClick}>
-//       <ModalStyle>
-//         <img src={largeImageURL} alt="Pixabay" />
-//       </ModalStyle>
-//     </OverlayStyle>,
-//     modalRoot
-//   );
-// };
-// export default Modal;
+  return createPortal(
+    <OverlayStyle onClick={onBackdropClick}>
+      <ModalStyle>
+        <img src={largeImageURL} alt="Pixabay" />
+      </ModalStyle>
+    </OverlayStyle>,
+    modalRoot
+  );
+};
+export default Modal;
